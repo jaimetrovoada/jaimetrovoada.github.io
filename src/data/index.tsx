9c49c97d43b746ae31b7668d67a753e5505f09b0
Jaime Trovoada <39800545+jaimetrovoada@users.noreply.github.com>
2022-07-12 11:29:06 +0800

    fix failing build

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import InstagramIcon from '@mui/icons-material/Instagram'
+// import InstagramIcon from '@mui/icons-material/Instagram'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail'
 import GitHubIcon from '@mui/icons-material/GitHub'
