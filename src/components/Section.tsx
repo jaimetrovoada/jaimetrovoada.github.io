c42aa4f13468b982239a386eef43084639183fb1
jaimetrovoada <jaimetrovoada@gmail.com>
2022-07-22 18:33:04 +0800

    add padding to sections

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -13,15 +13,14 @@ interface SectionProps extends BoxProps {
 const Section: React.FC<SectionProps> = ({ sectionTitle, children, fixedHeader, titleSize, ...props }) => {
   return (
     <Box
-      sx={
-        {
-          // height: '100vh',
-          // maxHeight: '100vh',
-          // width: { xs: '100%', md: '50%' },
-          // maxWidth: { xs: '100%', md: '50%' },
-          // flex: '0 0 100vh',
-        }
-      }
+      sx={{
+        padding: '50px 0',
+        // height: '100vh',
+        // maxHeight: '100vh',
+        // width: { xs: '100%', md: '50%' },
+        // maxWidth: { xs: '100%', md: '50%' },
+        // flex: '0 0 100vh',
+      }}
       {...props}
     >
       <Typography color="primary.main" variant="h4" fontSize={titleSize} marginBottom="1.5rem">
