33ef2e50b565cd5cfd1941cc2e6dcc89f76afb04
jaimetrovoada <jaimetrovoada@gmail.com>
2022-12-28 11:40:38 +0800

    improve table spacings

--- components/views/experience.tsx
+++ components/views/experience.tsx
@@ -9,20 +9,20 @@ export default function Experience() {
   return (
     <section className="flex flex-col gap-4">
       <h2 className="font-bold text-3xl"> &gt; Experience</h2>
-      <div className="flex flex-row h-80 max-h-80 w-full max-w-screen-xl mx-auto  p-4">
+      <div className="flex flex-row h-80 max-h-80 w-full max-w-screen-xl mx-auto">
         <div className="flex flex-col flex-none basis-1/4">
           {works.map((work, index) => (
             <button
               key={`${work.position}+${work.company}`}
               onClick={() => handleClick(index)}
-              className="bg-background-secondary-75 p-2"
+              className="bg-background-secondary-75 p-1 rounded-md"
             >
               {work.company}
             </button>
           ))}
         </div>
         <div className="flex-none basis-3/4 px-4">
-          <div>
+          <div className="m mb-4">
             <p className="font-bold">
               {work.position} @{" "}
               <span className="text-header-secondary font-normal">
@@ -30,8 +30,8 @@ export default function Experience() {
               </span>
             </p>
             <p className="text-foreground-secondary">{work.period}</p>
-            <p className="text-foreground-secondary">{work.description}</p>
           </div>
+          <p className="text-foreground-secondary">{work.description}</p>
         </div>
       </div>
     </section>
