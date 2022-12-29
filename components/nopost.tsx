155f46a3331ec2fcb6ff67fc875a971cc03e4e95
jaimetrovoada <jaimetrovoada@gmail.com>
2022-12-29 13:46:10 +0800

    create no posts component

---
+++ components/nopost.tsx
@@ -0,0 +1,7 @@
+export default function Wow() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <p className="font-bold text-5xl">WOW!!! Such Empty</p>
+    </div>
+  );
+}
