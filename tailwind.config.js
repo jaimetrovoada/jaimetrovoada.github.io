2505638ebd60aff99d2cc4f3e9acd687e08e013c
jaimetrovoada <jaimetrovoada@gmail.com>
2022-12-29 14:53:05 +0800

    add tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      "background": 'rgb(var(--color-background))',
-      "background-secondary": 'rgb(var(--color-background-secondary))',
-      "background-secondary-75": 'rgb(var(--color-background-secondary) / 0.75)',
-      "foreground": 'rgb(var(--color-foreground))',
-      "foreground-secondary": 'rgb(var(--color-foreground-secondary))',
-      "header-primary":"rgb(var(--color-section-header))",
-      "header-secondary":"rgb(var(--color-project-header))"
+    extend: {
+      colors: {
+        background: "rgb(var(--color-background))",
+        "background-secondary": "rgb(var(--color-background-secondary))",
+        "background-secondary-75":
+          "rgb(var(--color-background-secondary) / 0.75)",
+        foreground: "rgb(var(--color-foreground))",
+        "foreground-secondary": "rgb(var(--color-foreground-secondary))",
+        "header-primary": "rgb(var(--color-section-header))",
+        "header-secondary": "rgb(var(--color-project-header))",
+      },
     },
   },
-  plugins: [],
-  darkMode: 'class',
-}
+  plugins: [require("@tailwindcss/typography")],
+  darkMode: "class",
+};
