c943619db1cffbc590bb57120f024fbfd5ab61c4
jaimetrovoada <jaimetrovoada@gmail.com>
2022-12-31 14:20:45 +0800

    add markdown to html function

---
+++ helpers/mdtohtml.tsx
@@ -0,0 +1,8 @@
+import { remark } from "remark";
+import remarkParse from "remark-parse";
+import html from "remark-html";
+
+export default async function markdownToHtml(markdown: string) {
+  const result = await remark().use(remarkParse).use(html).process(markdown);
+  return result.toString();
+}
