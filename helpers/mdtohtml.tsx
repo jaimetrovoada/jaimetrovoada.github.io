7c9ccb6029bef2912fec2eaa6abbc9e8b9d8bee1
jaimetrovoada <jaimetrovoada@gmail.com>
2023-01-01 20:07:40 +0800

    add new remark and rehype plugins
    for extra md syntax support

--- helpers/mdtohtml.tsx
+++ helpers/mdtohtml.tsx
@@ -4,12 +4,25 @@ import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import remarkPrism from "remark-prism";
+import remarkMath from "remark-math";
+import {
+  remarkDefinitionList,
+  defListHastHandlers,
+} from "remark-definition-list";
+import rehypeKatex from "rehype-katex";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkPrism)
-    .use(remarkRehype)
+    .use(remarkMath)
+    .use(remarkDefinitionList)
+    .use(remarkRehype, {
+      handlers: {
+        ...defListHastHandlers,
+      },
+    })
+    .use(rehypeKatex)
     .use(remarkGfm)
     .use(rehypeStringify)
     .process(markdown);
