ba4252ab9f46b26aec4c2d1118d893603716dc9b
jaimetrovoada <jaimetrovoada@gmail.com>
2023-01-02 18:19:24 +0800

    change twitter:card type

--- components/layout.tsx
+++ components/layout.tsx
@@ -47,7 +47,7 @@ export default function Layout({ children }: LayoutProps) {
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
