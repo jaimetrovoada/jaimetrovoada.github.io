72ad30410b0293d5553e1b3430184727e08429a0
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-26 12:59:43 +0800

    add bg to sections

--- components/section.tsx
+++ components/section.tsx
@@ -5,10 +5,10 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 }
 export default function Section({ children, className }: Props) {
   return (
-    <section className={className}>
-      <div className={`container mx-auto h-full px-4 flex flex-col gap-6`}>
-        {children}
-      </div>
+    <section
+      className={`${className} container mx-auto flex h-full flex-col gap-6 rounded-2xl bg-background p-4`}
+    >
+      {children}
     </section>
   );
 }
