39d1d72e45cd555efff18693894ecf706eb3bb87
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-26 17:16:31 +0800

    fix project section

--- components/section.tsx
+++ components/section.tsx
@@ -6,7 +6,9 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 export default function Section({ children, className }: Props) {
   return (
     <section
-      className={`${className} container mx-auto flex h-full flex-col gap-6 rounded-2xl bg-background p-4`}
+      className={`${
+        className || ""
+      } container mx-auto flex flex-col gap-6 rounded-2xl bg-background p-4`}
     >
       {children}
     </section>
