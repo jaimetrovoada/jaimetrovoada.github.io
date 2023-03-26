16770bd1c4a292b36c4605137da6e13ff2d2773d
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-26 12:52:36 +0800

    create new layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { aboutMe, meta } from "../data";
 import Link from "next/link";
 import { useTheme } from "next-themes";
@@ -7,11 +7,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 import Prism from "prismjs";
 import Head from "next/head";
-import router from "next/router";
+import { useRouter } from "next/router";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faAt, faLocationPin } from "@fortawesome/free-solid-svg-icons";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
+
+const SocialIcon: React.FC<{ social: string }> = ({ social }) => {
+  switch (social) {
+    case "Email":
+      return (
+        <FontAwesomeIcon icon={faAt} className="text-foreground-secondary" />
+      );
+    case "GitHub":
+      return (
+        <FontAwesomeIcon
+          icon={faGithub}
+          className="text-foreground-secondary"
+        />
+      );
+
+    default:
+      return (
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          className="text-foreground-secondary"
+        />
+      );
+  }
+};
 export default function Layout({ children }: LayoutProps) {
   // const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
@@ -30,6 +56,8 @@ export default function Layout({ children }: LayoutProps) {
     const date = new Date();
     return date.getFullYear();
   };
+
+  const router = useRouter();
 
   return (
     <>
@@ -50,40 +78,70 @@ export default function Layout({ children }: LayoutProps) {
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-screen flex flex-col">
-        <header className="fixed px-4 py-2 bg-background-secondary-75 backdrop-blur-md h-16 z-10 w-screen">
-          <div className="flex flex-row justify-between items-center container mx-auto">
-            <div>
-              <Link href="/" className="flex flex-row items-center">
-                <div>
-                  <Image
-                    src="/images/avatar.webp"
-                    alt={"avatar"}
-                    width={50}
-                    height={50}
-                  />
-                </div>
-                <h1 className="font-bold text-xl">{aboutMe.name}</h1>
-              </Link>
-            </div>
-            <div>
+      <div className="flex max-h-screen min-h-screen flex-col gap-4 p-4 md:flex-row md:p-8">
+        <aside className="flex h-fit basis-1/3 flex-col gap-4 rounded-2xl bg-background p-4 transition-all">
+          <Link href="/" className="flex flex-row items-center gap-2">
+            <Image
+              src={aboutMe.avatar as string}
+              alt={"avatar"}
+              width={50}
+              height={50}
+            />
+            <h1 className="bg-gradient-to-r from-header-primary to-header-secondary bg-clip-text text-xl font-bold text-transparent md:-skew-y-6 md:text-5xl">
+              &lt;{aboutMe.name} /&gt;
+            </h1>
+          </Link>
+          <h2 className="text-lg font-bold text-foreground">
+            {aboutMe.occupation}
+          </h2>
+
+          {router.asPath.includes("/blog") ? null : (
+            <>
               <Link
                 href="/blog"
                 className="font-bold text-header-secondary underline"
               >
                 Blog
               </Link>
-            </div>
-          </div>
-        </header>
-        <main className="flex-1 overflow-y-auto flex flex-col gap-14">
+              <div>
+                <p className="font-bold">Get in touch:</p>
+                <div className="flex flex-row gap-2">
+                  {aboutMe.socials.map((media) => (
+                    <Link
+                      href={media.link}
+                      key={media.name}
+                      className="flex items-center gap-1 underline"
+                    >
+                      <SocialIcon social={media.name} />
+                      {media.name}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+              <div>
+                <p className="font-bold">My Skills:</p>
+                <span className="">{aboutMe.skills.join(", ").toString()}</span>
+              </div>
+              <Link
+                href={aboutMe.resumeLink as string}
+                className="font-bold text-header-secondary underline"
+              >
+                Resume
+              </Link>
+              <p className="text-foreground-secondary">
+                <FontAwesomeIcon icon={faLocationPin} /> {aboutMe.location}
+              </p>
+            </>
+          )}
+          <footer className="mt-14 hidden flex-row justify-around bg-background-secondary p-4 md:flex">
+            <p>&copy; {getYear()} - Jaime Trovoada</p>
+          </footer>
+        </aside>
+        <main className="flex flex-col gap-14 rounded-2xl bg-background p-4">
           {children}
         </main>
-        <footer className="p-4 flex flex-row justify-around bg-background-secondary mt-14">
-          <p>&copy; {getYear()} - Jaime Trovoada</p>
-        </footer>
         <button
-          className="bg-background-secondary fixed bottom-6 right-6 text-2xl rounded-full py-2 px-3"
+          className="fixed bottom-6 right-6 rounded-full bg-background-secondary py-2 px-3 text-2xl"
           onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
           aria-label="theme-switcher"
         >
@@ -93,6 +151,9 @@ export default function Layout({ children }: LayoutProps) {
             <FontAwesomeIcon icon={faMoon} />
           )}
         </button>
+        <footer className="mt-14 flex flex-row justify-around bg-background-secondary p-4 md:hidden">
+          <p>&copy; {getYear()} - Jaime Trovoada</p>
+        </footer>
       </div>
     </>
   );
--- components/views/intro.tsx
+++ components/views/intro.tsx
@@ -5,68 +5,11 @@ import { faAt, faLocationPin } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Section from "../section";
 
-const getFaIcon = (name: string): React.ReactNode => {
-  switch (name) {
-    case "Email":
-      return (
-        <FontAwesomeIcon icon={faAt} className="text-foreground-secondary" />
-      );
-    case "GitHub":
-      return (
-        <FontAwesomeIcon
-          icon={faGithub}
-          className="text-foreground-secondary"
-        />
-      );
-
-    default:
-      return (
-        <FontAwesomeIcon
-          icon={faLinkedin}
-          className="text-foreground-secondary"
-        />
-      );
-  }
-};
 export default function Intro() {
   return (
     <Section>
-      <h2 className="font-bold text-3xl">&gt; About Me</h2>
+      <h2 className="text-3xl font-bold">&gt; About Me</h2>
       <p>{aboutMe.introduction}</p>
-      <p>
-        My skills include:{" "}
-        <span className="font-bold text-header-secondary">
-          {aboutMe.skills.join(", ").toString()}
-        </span>
-      </p>
-      <p>
-        Here is where you can find my{" "}
-        <Link
-          href={aboutMe.resumeLink as string}
-          className="underline font-bold text-header-secondary"
-        >
-          resume
-        </Link>
-      </p>
-      <div>
-        <p>If you want to reach out, here is where to find me:</p>
-        <div className="flex flex-row gap-2">
-          {aboutMe.socials.map((media) => (
-            <Link
-              href={media.link}
-              key={media.name}
-              className="underline flex items-center gap-1"
-            >
-              {getFaIcon(media.name)}
-              {media.name}
-            </Link>
-          ))}
-        </div>
-      </div>
-      <p className="text-foreground-secondary">
-        {" "}
-        <FontAwesomeIcon icon={faLocationPin} /> {aboutMe.location}
-      </p>
     </Section>
   );
 }
