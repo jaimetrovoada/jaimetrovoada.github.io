c8f5c077f6c2775fc3abcba425bbbc0d2026cb2b
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-26 13:36:05 +0800

    change blogs page layout

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,9 +3,9 @@ import { readFileSync, readdirSync } from "fs";
 import matter from "gray-matter";
 import Link from "next/link";
 import path from "path";
-import Wow from "../../components/nopost";
 import { Frontmatter } from "../../types";
-import BlogBody from "../../components/blogs";
+import Head from "next/head";
+import { meta } from "../../data";
 
 interface PostProps {
   slug: string;
@@ -16,13 +16,13 @@ function Card({ post }: { post: PostProps }) {
   return (
     <Link
       href={`/blog/${post.slug}`}
-      className="flex flex-col gap-3 bg-background-secondary p-5 rounded md:hover:shadow-[5px_5px_0_0_theme(colors.foreground)] md:transition-all md:hover:shadow-foreground md:hover:-translate-y-1 md:hover:-translate-x-1 hover:underline"
+      className="flex flex-row gap-3 rounded bg-background p-5 hover:underline md:transition-all md:hover:-translate-y-1 md:hover:-translate-x-1 md:hover:shadow-[5px_5px_0_0_theme(colors.foreground)] md:hover:shadow-foreground"
     >
       <img
         src={`https://jaimetrovoada.vercel.app/api/og?title=${post.frontmatter.title}`}
-        width="100%"
-        height={50}
-        className="object-cover aspect-auto"
+        width={100}
+        height={100}
+        className="aspect-auto object-cover"
         alt="post image preview"
       />
       <h2 className="text-xl font-bold text-header-secondary">
@@ -33,25 +33,39 @@ function Card({ post }: { post: PostProps }) {
 }
 
 export default function Posts({ posts }: { posts: PostProps[] }) {
-  if (posts.length === 0) {
-    return (
-      <>
-        <BlogBody>
-          <Wow />
-        </BlogBody>
-      </>
-    );
-  }
-
   return (
     <>
-      <BlogBody>
-        <div className="grid gap-4 md:grid-cols-3 xl:grid-cols-4">
-          {posts.map((post, index) => {
+      <Head>
+        <title>Blog | Jaime Trovoada</title>
+        <meta name="description" content={meta.description.blog} />
+        <meta name="keywords" content={meta.keywords.blog} />
+        <meta property="og:description" content={meta.description.blog} />
+        <meta property="og:title" content="Blog | Jaime Trovoada" />
+        <meta
+          property="og:image"
+          content={`https://jaimetrovoada.vercel.app/api/og?title=Blog | Jaime Trovoada`}
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta name="twitter:title" content="Blog | Jaime Trovoada" />
+        <meta name="twitter:description" content={meta.description.blog} />
+        <meta
+          name="twitter:image"
+          content={`https://jaimetrovoada.vercel.app/api/og?title=Blog | Jaime Trovoada`}
+        />
+      </Head>
+      <div className="flex flex-col gap-8">
+        {posts && posts.length ? (
+          posts.map((post, index) => {
             return <Card post={post} key={`${index}-${post.slug}`} />;
-          })}
-        </div>
-      </BlogBody>
+          })
+        ) : (
+          <>
+            <div className="mx-auto text-3xl font-bold">
+              WOW!!! SUCH EMPTY!!!
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
