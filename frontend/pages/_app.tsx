f0a86e298d790f8f2b1f1547d003f019d5b3df4c
jaimetrovoada <jaimetrovoada@gmail.com>
2023-04-18 20:33:48 +0800

    add .env to gitignore

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,22 +5,15 @@ import "prismjs/themes/prism-tomorrow.css";
 import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { PrismicPreview } from "@prismicio/next";
-import { PrismicProvider } from "@prismicio/react";
-import Link from "next/link";
-import { repositoryName } from "../prismicio";
 
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
-      <PrismicPreview repositoryName={repositoryName}></PrismicPreview>
-      <ThemeProvider attribute="class">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </PrismicProvider>
+    <ThemeProvider attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
