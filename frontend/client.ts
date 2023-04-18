0fe82bcabd288acf68b7e0c1e863fd79fea3898d
jaimetrovoada <jaimetrovoada@gmail.com>
2023-04-18 20:49:57 +0800

    add apiVersion to client

--- frontend/client.ts
+++ frontend/client.ts
@@ -4,4 +4,5 @@ export default createClient({
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_PROJECT_DATASET,
   useCdn: true,
+  apiVersion: "2023-04-04",
 });
