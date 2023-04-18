75f569872eb0829c94b2349994b3f6ef1a148c38
jaimetrovoada <jaimetrovoada@gmail.com>
2023-04-18 20:38:27 +0800

    create fetch functions

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,115 +1,35 @@
-import axios from "axios";
-import { createClient } from "../prismicio";
-import type { Content } from "@prismicio/client";
+import client from "../client";
 
-export interface PostsResponse {
-  data: PostsResponseData;
-  extensions: Extensions;
-}
-
-export interface PostsResponseData {
-  posts: PostsResponsePosts;
-}
-
-export interface PostsResponsePosts {
-  nodes: PostsResponseNode[];
-}
-
-export interface PostsResponseNode {
-  excerpt: string;
-  slug: string;
-  title: string;
-}
-
-export interface Extensions {
-  debug: Debug[];
-}
-
-export interface Debug {
-  type: string;
-  message: string;
-}
-
-export interface PostBySlugResponse {
-  data: PostBySlugData;
-  extensions: Extensions;
-}
-
-export interface PostBySlugData {
-  post: PostBySlugPost;
-}
-
-export interface PostBySlugPost {
+export interface SanityPost {
+  author: Author;
   content: string;
+  keywords: string;
   slug: string;
   title: string;
+  description: string
 }
 
-const API_URL = process.env.NEXT_PUBLIC_WPGRAPHQL_URL as string;
+export interface Author {
+  name: string
+}
 
-async function fetchAPI<T>(query = "", variables: Record<string, any> = {}) {
-  const headers = { "Content-Type": "application/json" };
-
-  const res = await axios.post<T>(
-    API_URL,
-    {
-      query,
-      variables,
-    },
-    {
-      headers,
-    }
+const getPosts = async () => {
+  const posts = await client.fetch<Pick<SanityPost, "slug" | "title">[]>(
+    `*[_type == "post" && defined(slug.current)]{"slug": slug.current, title}`
   );
 
-  return res.data;
-}
-
-export async function getPosts(first = 10) {
-  const res = await fetchAPI<PostsResponse>(
-    `query FetchPosts($first: Int = 10) {
-        posts(first: $first) {
-          nodes {
-            excerpt
-            slug
-            title
-          }
-        }
-      }`,
-    { first }
-  );
-
-  return res.data.posts.nodes;
-}
-
-export async function getPostBySlug(slug: string) {
-  console.log({ slug });
-  const id = slug;
-  const res = await fetchAPI<PostBySlugResponse>(
-    `query GetPost($id: ID = "") {
-    post(id: $id, idType: SLUG) {
-      content
-      slug
-      title
-    }
-  }`,
-    { id }
-  );
-
-  return res.data.post;
-}
-
-const getAllPosts = async () => {
-  const client = createClient();
-  const pages = await client.getAllByType<Content.BlogDocument>("blog");
-
-  return pages;
+  return posts;
 };
 
-const getPostByUid = async (uid: string) => {
-  const client = createClient();
-  const page = await client.getByUID<Content.BlogDocument>("blog", uid);
+const getPostBySlug = async (slug: string) => {
+  const post = await client.fetch<SanityPost>(
+    `
+    *[_type == "post" && slug.current == $slug][0]{title, "slug": slug.current, content, author->{name}, keywords, description}
+  `,
+    { slug }
+  );
 
-  return page;
+  return post;
 };
 
-export default { getPosts, getPostBySlug, getAllPosts, getPostByUid };
+export default { getPosts, getPostBySlug };
