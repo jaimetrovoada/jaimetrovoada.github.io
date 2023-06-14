1a157942bb10e2a5e271031036e265f75205dba3
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-14 16:42:21 +0800

    create multiple root layouts for studio and portfolio

--- app/(portfolio)/layout.tsx
+++ app/(portfolio)/layout.tsx
@@ -1,10 +1,55 @@
-import React from "react";
 import Layout from "@/components/layout";
+import { Metadata } from "next";
+import { meta } from "@/data";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
-interface Props {
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: meta.title,
+  description: meta.description.home,
+  keywords: meta.keywords.home,
+  openGraph: {
+    title: meta.title,
+    description: meta.description.home,
+    images: [
+      {
+        url: `https://jaimetrovoada.vercel.app/api/og?title=Jaime%20Trovoada`,
+        type: "image/png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: meta.title,
+    description: meta.description.home,
+    images: [
+      {
+        url: `https://jaimetrovoada.vercel.app/api/og?title=Jaime%20Trovoada`,
+        alt: "Jaime's portfolio",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
-}
-
-export default function PortfolioLayout({ children }: Props) {
-  return <Layout>{children}</Layout>;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={
+          inter.className +
+          " flex max-h-screen min-h-screen max-w-full flex-col gap-4 overflow-y-auto bg-gray-950 p-4 md:flex-row md:overflow-hidden md:p-8"
+        }
+      >
+        <Layout>{children}</Layout>
+      </body>
+    </html>
+  );
 }
