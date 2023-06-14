95ed78c6085c7ef870c64de9d66a8840dca393d4
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-14 15:16:37 +0800

    modify styles

--- components/section.tsx
+++ components/section.tsx
@@ -8,7 +8,7 @@ export default function Section({ children, className }: Props) {
     <section
       className={`${
         className || ""
-      } container mx-auto flex flex-col gap-6 rounded-2xl bg-background p-4`}
+      } flex flex-col gap-6 rounded-2xl bg-white p-4 shadow-md`}
     >
       {children}
     </section>
