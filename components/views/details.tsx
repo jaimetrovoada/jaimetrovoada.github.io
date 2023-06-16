cfd4848130f4803c550ea420a5bcc8a4ddd71197
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-16 13:37:01 +0800

    update styles

--- components/views/details.tsx
+++ components/views/details.tsx
@@ -7,12 +7,12 @@ import { Linkedin, GitHub, AtSign, MapPin, ArrowRight } from "react-feather";
 const SocialIcon: React.FC<{ social: string }> = ({ social }) => {
   switch (social) {
     case "Email":
-      return <AtSign size={16} />;
+      return <AtSign size={14} />;
     case "GitHub":
-      return <GitHub size={16} />;
+      return <GitHub size={14} />;
 
     default:
-      return <Linkedin size={16} />;
+      return <Linkedin size={14} />;
   }
 };
 
@@ -29,58 +29,68 @@ const Details = ({ resumeUrl }: Props) => {
   };
 
   return (
-    <aside className="flex h-fit basis-1/3 flex-col gap-4 rounded-2xl bg-gray-800/50 p-4 text-slate-200 shadow-md transition-all">
-      <Link href="/" className="mx-auto rounded-full bg-gray-700/25 p-2">
-        <Image
-          src={aboutMe.avatar as string}
-          alt={"avatar"}
-          width={100}
-          height={100}
-        />
-      </Link>
-      <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold text-yellow-400 md:text-4xl">
-          <Link href="/">{aboutMe.name}</Link>
-        </h1>
-        <h2 className="text-lg font-semibold text-blue-400">
-          {aboutMe.occupation}
-        </h2>
-      </div>
-
-      <div className="flex w-fit flex-row items-center gap-2 rounded-full bg-gray-600/50 p-2 text-xs leading-none text-gray-200">
-        <MapPin size={12} />
-        <span>{aboutMe.location}</span>
-      </div>
-
-      <div className={`${path?.includes("/blog") && "hidden"} md:block`}>
-        <p className="font-semibold">Get in touch:</p>
-        <div className="flex flex-row gap-2">
-          {aboutMe.socials.map((media) => (
-            <Link
-              href={media.link}
-              key={media.name}
-              className="flex items-center gap-1 text-slate-300 underline"
-            >
-              <SocialIcon social={media.name} />
-              {media.name}
-            </Link>
-          ))}
+    <aside className="flex h-fit basis-1/3 flex-col gap-10 rounded-2xl bg-gray-800/50 p-4 text-slate-200 shadow-md transition-all">
+      <div className="flex flex-col gap-4">
+        <Link href="/" className="mx-auto rounded-full bg-gray-700/25 p-2">
+          <Image
+            src={aboutMe.avatar as string}
+            alt={"avatar"}
+            width={100}
+            height={100}
+          />
+        </Link>
+        <div>
+          <h1 className="text-2xl font-bold text-yellow-400 md:text-4xl">
+            <Link href="/">{aboutMe.name}</Link>
+          </h1>
+          <h2 className="text-lg font-semibold text-blue-400">
+            {aboutMe.occupation}
+          </h2>
         </div>
       </div>
-      <div className={`${path?.includes("/blog") && "hidden"} md:block`}>
-        <p className="font-semibold">My Skills:</p>
-        <div className="flex flex-row flex-wrap gap-2">
-          {aboutMe.skills.map((skill) => (
-            <span
-              key={skill}
-              className="rounded-full bg-gray-600/50 p-2 text-xs leading-none text-gray-200"
-            >
-              {skill}
-            </span>
-          ))}
+
+      <div
+        className={`flex-col gap-4 ${
+          path?.includes("/blog") ? "hidden md:flex" : "flex"
+        }`}
+      >
+        <div>
+          <p className="font-semibold">Get in touch:</p>
+          <div className="flex flex-row gap-2">
+            {aboutMe.socials.map((media) => (
+              <Link
+                href={media.link}
+                key={media.name}
+                className="flex items-center gap-1 text-sm text-slate-300 underline"
+              >
+                <SocialIcon social={media.name} />
+                {media.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <p className="font-semibold">My Skills:</p>
+          <div className="flex flex-row flex-wrap gap-2">
+            {aboutMe.skills.map((skill) => (
+              <span
+                key={skill}
+                className="rounded-full bg-gray-600/25 p-2 text-xs leading-none text-slate-300"
+              >
+                {skill}
+              </span>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex w-fit flex-row items-center gap-2 rounded-full  text-sm leading-none text-slate-300">
+          <MapPin size={14} />
+          <span>{aboutMe.location}</span>
         </div>
       </div>
-      <div className="mt-8 flex flex-row gap-4">
+
+      <div className="flex flex-row gap-4">
         <Link
           href="/blog"
           className="inline-flex items-center text-lg font-semibold text-header-secondary underline"
