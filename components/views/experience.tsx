10d40e7b4ef6b99f6180b8b55012c86bfd839060
jaimetrovoada <jaimetrovoada@gmail.com>
2023-07-24 12:31:10 +0800

    change work table to timeline

--- components/views/experience.tsx
+++ components/views/experience.tsx
@@ -1,14 +1,6 @@
-"use client";
-import { useState } from "react";
 import { works } from "../../data";
 import Section from "../section";
-import { WorkProps } from "../../types";
 export default function Experience() {
-  const [work, setWork] = useState<WorkProps>(works[0]);
-
-  const handleClick = (index: number) => {
-    setWork(works[index]);
-  };
 
   return (
     <Section>
@@ -16,31 +8,23 @@ export default function Experience() {
         {" "}
         &gt; Experience
       </h2>
-      <div className="mx-auto flex h-80 max-h-80 w-full max-w-screen-xl flex-col md:flex-row">
-        <div className="flex flex-none basis-1/4 flex-col">
-          {works.map((work, index) => (
-            <button
-              key={`${work.position}+${work.company}`}
-              onClick={() => handleClick(index)}
-              className="rounded-md bg-gray-600/50 p-2 leading-none text-gray-200 hover:bg-gray-600/75"
-            >
-              {work.company}
-            </button>
-          ))}
-        </div>
-        <div className="flex-none basis-3/4 px-4">
-          <div className="m mb-4">
-            <p>
-              <span className="mr-2 font-semibold underline">
-                {work.position}
-              </span>
-              <span className="text-slate-300">@{work.company}</span>
-            </p>
-            <p className="text-slate-400">{work.period}</p>
-          </div>
-          <p className="text-slate-300">{work.description}</p>
-        </div>
-      </div>
+      <ol className="border-l border-neutral-500">
+        {works.map((work) => (
+          <li key={`${work.position}+${work.company}`}>
+            <div className="flex-start flex items-center pt-3">
+              <div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-500"></div>
+              <p className="text-sm text-slate-300">
+                {work.period}
+              </p>
+            </div>
+            <div className="mb-6 ml-4 mt-2">
+              <h4 className="mb-1.5 text-lg font-semibold">
+                {work.position} @ {work.company}
+              </h4>
+            </div>
+          </li>
+        ))}
+      </ol>
     </Section>
   );
 }
